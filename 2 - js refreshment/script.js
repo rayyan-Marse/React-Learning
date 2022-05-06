f6a0c741d7b253f8c97bc3af7addf3fa0237971a
rayyan <rayyansto@gmail.com>
2022-05-06 08:35:12 +0530

    classes in js version ES6

--- 2 - js refreshment/script.js
+++ 2 - js refreshment/script.js
@@ -8,3 +8,99 @@ const name1 = "rayyan shaikh";
 name1 = 'data';
 
 console.log(name1);
+
+// arow functions diffrent writing types
+console.log('rayyan shaikh');
+
+function print1(){
+  console.log("rayyan");
+}
+
+print1();
+
+// arow functions
+
+const print2 = ()=>{
+  console.log('rayyan2');
+}
+
+print2();
+  
+  const print3 = number =>{
+    console.log(number);
+  }
+    
+    print3(25);
+   
+const print4 = number => number*10;
+
+console.log(print4(25));
+
+// classes in js and inheritance
+class Parent{
+ 
+    constructor(){
+     this.gender = 'male';
+   }
+   
+    printGender(){
+     
+     console.log(this.gender);
+    }
+   
+   
+   }
+   
+   
+   class Person extends Parent{
+     constructor(){
+     super();
+     this.name = 'rayyan shaikh ';
+     }
+    
+    printName(){
+     console.log(this.name);
+   }
+   
+   }
+    
+   const p1 = new  Person();
+   p1.printName();
+   p1.printGender();
+
+      
+  //ES6 features classes without constructor
+
+// classes in js and inheritance
+class Parent1{
+ 
+
+    gender = 'male';
+  
+  
+   printGender=()=>{
+    
+    console.log(this.gender);
+   }
+  
+  
+  }
+  
+  
+  class Person2 extends Parent1{
+    
+   
+    name = 'rayyan shaikh2 ';
+   
+   
+   printName = ()=>{
+    console.log(this.name);
+  }
+  
+  }
+   
+  const p2 = new  Person2();
+  p2.printName();
+  p2.printGender();
+  
+   
